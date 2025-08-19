90583ce23581fddc18181c4a149536d8b19d33fc
Dan Johnson <dan@try.be>
2025-08-19 21:48:46 +0100

    chore: add config

---
+++ config/pinout.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /**
+     * Here you must define which GPIO pins your application is going to use,
+     * and the direction each one will have (input or output).
+     */
+    'pins' => [
+        // For example,
+        // 1 => Direction::OUTPUT,
+        // 2 => Direction::INPUT,
+    ],
+
+    'php_bin' => '/usr/bin/php',
+
+    'gpio_listener' => [
+        'tmp_dir' => '/tmp/pinout/',
+        'pid' => 1,
+    ],
+
+    'sys_file' => \DanJohnson95\Pinout\Shell\LibGPIOD::class,
+    'gpio_chip' => 'gpiochip0',
+];
