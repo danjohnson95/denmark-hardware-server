29d3188dc7220a9913ecd4291b2a9ac1da62f95c
Dan Johnson <dan@try.be>
2025-08-19 21:45:45 +0100

    chore: register events

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Jobs\CheckNetwork;
+use App\Jobs\CheckNgrok;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::job(new CheckNetwork)->everySecond();
+Schedule::job(new CheckNgrok)->everySecond();
